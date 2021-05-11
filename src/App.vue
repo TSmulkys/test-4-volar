2e43ecd26948eb81fe465940fcff1299009c5a79
Tomas Smulkys <tsmulkys@gmail.com>
2021-05-11 15:08:32 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+div(class="app-class")
+  TestComp(:text="loremText")
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+// Components
+import TestComp from "./components/test-comp.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: { TestComp },
+  setup() {
+    const loremText = ref('Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, quaerat? Incidunt, rem fugiat a deserunt aliquam qui vel quidem. Deleniti nihil est rerum, eius adipisci mollitia dicta amet distinctio inventore?')
+
+    return {
+      loremText
+    }
+  }
+})
+</script>
+
+<style scoped lang="postcss">
+.app-class {
+  background-color: pink;
+}
+</style>
---
+++ src/components/test-comp.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+p {{ text }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TestComp',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    return {
+      //
+    };
+  },
+});
+</script>
+
+<style scoped lang="postcss"></style>
